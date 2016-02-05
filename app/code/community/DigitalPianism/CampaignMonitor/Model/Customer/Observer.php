97e7614c04eee22e6fd61ad35e0384f536c37d76
Raphael Petrini <raphael@factoryx.com.au>
2016-02-05 09:43:12 +0100

    support subscription via account creation checkbox

--- app/code/community/DigitalPianism/CampaignMonitor/Model/Customer/Observer.php
+++ app/code/community/DigitalPianism/CampaignMonitor/Model/Customer/Observer.php
@@ -10,32 +10,32 @@ class DigitalPianism_CampaignMonitor_Model_Customer_Observer
      * @param $observer
      */
     public function check_subscription_status($observer)
-    {           
-                
-        $event = $observer->getEvent();    
+    {
+
+        $event = $observer->getEvent();
         $customer = $event->getCustomer();
-		
-		if (Mage::helper('campaignmonitor')->isOAuth())
-		{
-			$accessToken = Mage::getModel('campaignmonitor/auth')->getAccessToken();
-			$refreshToken = Mage::getModel('campaignmonitor/auth')->getRefreshToken();
-			
-			$auth = array(
-						'access_token' => $accessToken,
-						'refresh_token' => $refreshToken
-					);
-		}
-		else
-		{
-			$auth = Mage::helper('campaignmonitor')->getApiKey();
-		}
-		
-		$listID = Mage::helper('campaignmonitor')->getListId();
-        
+
+        if (Mage::helper('campaignmonitor')->isOAuth())
+        {
+            $accessToken = Mage::getModel('campaignmonitor/auth')->getAccessToken();
+            $refreshToken = Mage::getModel('campaignmonitor/auth')->getRefreshToken();
+
+            $auth = array(
+                'access_token' => $accessToken,
+                'refresh_token' => $refreshToken
+            );
+        }
+        else
+        {
+            $auth = Mage::helper('campaignmonitor')->getApiKey();
+        }
+
+        $listID = Mage::helper('campaignmonitor')->getListId();
+
         $name = $customer->getFirstname() . " " . $customer->getLastname();
         $newEmail = $customer->getEmail();
-        $subscribed = $customer->getIsSubscribed();        
-        
+        $subscribed = $customer->getIsSubscribed();
+
         $oldEmail = Mage::getModel('customer/customer')->load($customer->getId())->getEmail();
 
         if (empty($oldEmail)) return;
@@ -43,20 +43,20 @@ class DigitalPianism_CampaignMonitor_Model_Customer_Observer
         // or the other), get the existing value from the database
         if($subscribed === NULL)
         {
-			$subscribed = Mage::getModel('newsletter/subscriber')->loadByCustomer($customer)->isSubscribed();
+            $subscribed = Mage::getModel('newsletter/subscriber')->loadByCustomer($customer)->isSubscribed();
         }
-        
+
         //print "Name: $name, New email: $newEmail, Subscribed: $subscribed, Old email: $oldEmail<br />\n";
-		if($auth && $listID)
+        if($auth && $listID)
         {
             $customFields = Mage::helper('campaignmonitor')->generateCustomFields($customer);
-            
-            try 
-			{
-				$client = new CS_REST_Subscribers($listID,$auth);
-            } 
-			catch(Exception $e) 
-			{
+
+            try
+            {
+                $client = new CS_REST_Subscribers($listID,$auth);
+            }
+            catch(Exception $e)
+            {
                 Mage::helper('campaignmonitor')->log("Error connecting to CampaignMonitor server: ".$e->getMessage());
                 return;
             }
@@ -72,55 +72,55 @@ class DigitalPianism_CampaignMonitor_Model_Customer_Observer
                 if ($oldEmail && $newEmail != $oldEmail)
                 {
                     Mage::helper('campaignmonitor')->log("Unsubscribing old email address: $oldEmail");
-                    try 
-					{
+                    try
+                    {
                         $result = $client->unsubscribe($oldEmail);
-						
-						if (!$result->was_successful()) {
-							// If you receive '121: Expired OAuth Token', refresh the access token
-							if ($result->response->Code == 121) {
-								// Refresh the token
-								Mage::helper('campaignmonitor')->refreshToken();
-							}
-							// Make the call again
-							$client->unsubscribe($oldEmail);
-						}
-                    } 
-					catch(Exception $e) 
-					{
+
+                        if (!$result->was_successful()) {
+                            // If you receive '121: Expired OAuth Token', refresh the access token
+                            if ($result->response->Code == 121) {
+                                // Refresh the token
+                                Mage::helper('campaignmonitor')->refreshToken();
+                            }
+                            // Make the call again
+                            $client->unsubscribe($oldEmail);
+                        }
+                    }
+                    catch(Exception $e)
+                    {
                         Mage::helper('campaignmonitor')->log("Error in SOAP call: ".$e->getMessage());
                         return;
                     }
                 }
-            
+
                 // Using 'add and resubscribe' rather than just 'add', otherwise
                 // somebody who unsubscribes and resubscribes won't be put back
                 // on the active list
                 Mage::helper('campaignmonitor')->log("Subscribing new email address: $newEmail");
-                try 
-				{
+                try
+                {
                     $result = $client->add(array(
+                        "EmailAddress" => $newEmail,
+                        "Name" => $name,
+                        "CustomFields" => $customFields,
+                        "Resubscribe" => true));
+
+                    if (!$result->was_successful()) {
+                        // If you receive '121: Expired OAuth Token', refresh the access token
+                        if ($result->response->Code == 121) {
+                            // Refresh the token
+                            Mage::helper('campaignmonitor')->refreshToken();
+                        }
+                        // Make the call again
+                        $client->add(array(
                             "EmailAddress" => $newEmail,
                             "Name" => $name,
                             "CustomFields" => $customFields,
-							"Resubscribe" => true));
-							
-					if (!$result->was_successful()) {
-						// If you receive '121: Expired OAuth Token', refresh the access token
-						if ($result->response->Code == 121) {
-							// Refresh the token
-							Mage::helper('campaignmonitor')->refreshToken();
-						}
-						// Make the call again
-						$client->add(array(
-								"EmailAddress" => $newEmail,
-								"Name" => $name,
-								"CustomFields" => $customFields,
-								"Resubscribe" => true));
-					}
-                } 
-				catch(Exception $e) 
-				{
+                            "Resubscribe" => true));
+                    }
+                }
+                catch(Exception $e)
+                {
                     Mage::helper('campaignmonitor')->log("Error in SOAP call: ".$e->getMessage());
                     return;
                 }
@@ -128,23 +128,23 @@ class DigitalPianism_CampaignMonitor_Model_Customer_Observer
             else
             {
                 Mage::helper('campaignmonitor')->log("Unsubscribing: $oldEmail");
-                
-                try 
-				{
+
+                try
+                {
                     $result = $client->unsubscribe($oldEmail);
-					
-					if (!$result->was_successful()) {
-						// If you receive '121: Expired OAuth Token', refresh the access token
-						if ($result->response->Code == 121) {
-							// Refresh the token
-							Mage::helper('campaignmonitor')->refreshToken();
-						}
-						// Make the call again
-						$client->unsubscribe($oldEmail);
-					}
-                } 
-				catch(Exception $e) 
-				{
+
+                    if (!$result->was_successful()) {
+                        // If you receive '121: Expired OAuth Token', refresh the access token
+                        if ($result->response->Code == 121) {
+                            // Refresh the token
+                            Mage::helper('campaignmonitor')->refreshToken();
+                        }
+                        // Make the call again
+                        $client->unsubscribe($oldEmail);
+                    }
+                }
+                catch(Exception $e)
+                {
                     Mage::helper('campaignmonitor')->log("Error in SOAP call: ".$e->getMessage());
                     return;
                 }
@@ -161,43 +161,114 @@ class DigitalPianism_CampaignMonitor_Model_Customer_Observer
         $customer = $event->getCustomer();
 
         if (Mage::helper('campaignmonitor')->isOAuth())
-		{
-			$accessToken = Mage::getModel('campaignmonitor/auth')->getAccessToken();
-			$refreshToken = Mage::getModel('campaignmonitor/auth')->getRefreshToken();
-			
-			$auth = array(
-						'access_token' => $accessToken,
-						'refresh_token' => $refreshToken
-					);
-		}
-		else
-		{
-			$auth = Mage::helper('campaignmonitor')->getApiKey();
-		}
-		
-		$listID = Mage::helper('campaignmonitor')->getListId();
-       
+        {
+            $accessToken = Mage::getModel('campaignmonitor/auth')->getAccessToken();
+            $refreshToken = Mage::getModel('campaignmonitor/auth')->getRefreshToken();
+
+            $auth = array(
+                'access_token' => $accessToken,
+                'refresh_token' => $refreshToken
+            );
+        }
+        else
+        {
+            $auth = Mage::helper('campaignmonitor')->getApiKey();
+        }
+
+        $listID = Mage::helper('campaignmonitor')->getListId();
+
         $email = $customer->getEmail();
 
         if($auth && $listID)
         {
             Mage::helper('campaignmonitor')->log("Customer deleted, unsubscribing: $email");
-            try 
-			{
+            try
+            {
                 $client = new CS_REST_Subscribers($listID,$auth);
                 $result = $client->unsubscribe($email);
-				if (!$result->was_successful()) {
-					// If you receive '121: Expired OAuth Token', refresh the access token
-					if ($result->response->Code == 121) {
-						// Refresh the token
-						Mage::helper('campaignmonitor')->refreshToken();
-					}
-					// Make the call again
-					$client->unsubscribe($email);
-				}
-            } 
-			catch(Exception $e) 
-			{
+                if (!$result->was_successful()) {
+                    // If you receive '121: Expired OAuth Token', refresh the access token
+                    if ($result->response->Code == 121) {
+                        // Refresh the token
+                        Mage::helper('campaignmonitor')->refreshToken();
+                    }
+                    // Make the call again
+                    $client->unsubscribe($email);
+                }
+            }
+            catch(Exception $e)
+            {
+                Mage::helper('campaignmonitor')->log("Error in SOAP call: ".$e->getMessage());
+                return;
+            }
+        }
+    }
+
+    /**
+     * Subscribe a customer who subscribed to Mage after a successfull registration
+     * @param $observer
+     */
+    public function subscribeCustomer(Varien_Event_Observer $observer)
+    {
+        // Get the customer
+        $customer = $observer->getEvent()->getCustomer();
+        // Get its email
+        $email = $customer->getEmail();
+        $name = $customer->getFirstname() . " " . $customer->getLastname();
+        // Check if subscribed
+        $hasSubscribed = $customer->getIsSubscribed();
+
+        if (Mage::helper('campaignmonitor')->isOAuth())
+        {
+            $accessToken = Mage::getModel('campaignmonitor/auth')->getAccessToken();
+            $refreshToken = Mage::getModel('campaignmonitor/auth')->getRefreshToken();
+
+            $auth = array(
+                'access_token' => $accessToken,
+                'refresh_token' => $refreshToken
+            );
+        }
+        else
+        {
+            $auth = Mage::helper('campaignmonitor')->getApiKey();
+        }
+
+        $listID = Mage::helper('campaignmonitor')->getListId();
+
+        // Check if the customer has subscribed via OG magento checkbox
+        if ($hasSubscribed)
+        {
+            $customFields = Mage::helper('campaignmonitor')->generateCustomFields($customer);
+
+            // Using 'add and resubscribe' rather than just 'add', otherwise
+            // somebody who unsubscribes and resubscribes won't be put back
+            // on the active list
+            Mage::helper('campaignmonitor')->log("Subscribing new email address: $newEmail");
+            try
+            {
+                $client = new CS_REST_Subscribers($listID,$auth);
+                $result = $client->add(array(
+                    "EmailAddress" => $email,
+                    "Name" => $name,
+                    "CustomFields" => $customFields,
+                    "Resubscribe" => true));
+
+                if (!$result->was_successful()) {
+                    // If you receive '121: Expired OAuth Token', refresh the access token
+                    if ($result->response->Code == 121) {
+                        // Refresh the token
+                        Mage::helper('campaignmonitor')->refreshToken();
+                    }
+                    // Make the call again
+                    $client->add(array(
+                        "EmailAddress" => $email,
+                        "Name" => $name,
+                        "CustomFields" => $customFields,
+                        "Resubscribe" => true));
+                }
+            }
+            catch(Exception $e)
+            {
                 Mage::helper('campaignmonitor')->log("Error in SOAP call: ".$e->getMessage());
                 return;
             }
